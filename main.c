91bcc20765bde2239ebdb6acb666e9fe79ccf6d8
nikarcosta <mrc32467098@outlook.com>
2025-08-15 18:49:46 -0300

    feat: add edit books feature

--- main.c
+++ main.c
@@ -52,6 +52,7 @@ int main(void) {
         printf("1 - Add book;\n");
         printf("2 - Delete book;\n");
         printf("3 - View books;\n");
+        printf("4 - Edit book;\n");
         printf("0 - Exit the application;\n");
 
         scanf("%d", &option);
@@ -65,6 +66,9 @@ int main(void) {
                 break;
             case 3:
                 view_books();
+                break;
+            case 4:
+                edit_book();
                 break;
             case 0:
                 run = 0;
@@ -248,7 +252,85 @@ void delete_book() {
     return_to_menu();
 }
 
-void edit_book(){}
+void edit_book() {
+    printf("*********************************************************************\n");
+    printf("****************************EDIT BOOKS*******************************\n");
+    printf("*********************************************************************\n");
+    getchar();
+
+    printf("Enter the title of the book you want to edit: \n");
+    char book_to_edit[256];
+    fgets(book_to_edit, sizeof(book_to_edit),stdin);
+    book_to_edit[strcspn(book_to_edit, "\n")] = '\0';
+
+    struct book *my_books = NULL;
+    int total_books = load_books(&my_books);
+
+    bool book_found = false;
+
+    printf("What do you want to edit? Type the number correspoding to the option.\n");
+    printf("1 - Book Title\n");
+    printf("2 - Book Author\n");
+    printf("3 - Book Price\n");
+
+    int edit_option;
+    scanf("%d", &edit_option);
+    getchar();
+
+    char title[100];
+    char author[50];
+    double price;
+
+    switch (edit_option) {
+        case 1:
+            getchar();
+            printf("Enter new title:\n");
+            fgets(title, sizeof(title), stdin);
+            title[strcspn(title, "\n")] = '\0';
+            break;
+        case 2:
+            getchar();
+            printf("Enter new author:\n");
+            fgets(author, sizeof(author), stdin);
+            author[strcspn(author, "\n")] = '\0';
+            break;
+        case 3:
+            printf("Enter new price:\n");
+            scanf("%lf", &price);
+            break;
+        default:
+            printf("Invalid option!");
+            return_to_menu();
+            return;
+    }
+
+    for (int i = 0; i < total_books; i++) {
+        if (strcmp(my_books[i].title, book_to_edit) == 0) {
+            book_found = true;
+
+            if (edit_option == 1) {
+                strcpy(my_books[i].title, title);
+            } else if (edit_option == 2) {
+                strcpy(my_books[i].author, author);
+            } else {
+                my_books[i].price = price;
+            }
+            break;
+        }
+
+    }
+
+    if (book_found) {
+        create_file(my_books, total_books);
+
+        printf("Book edited successfully!\n");
+    } else {
+        printf("Book not found!\n");
+    }
+
+    free(my_books);
+    return_to_menu();
+}
 
 int load_books(struct book **books_out) {
     FILE *fPtr = fopen(FILENAME, "r");
